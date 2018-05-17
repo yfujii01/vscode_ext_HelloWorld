1e76a6d35778d6a07e75395642482b0c36e4a738
yfujii01 <fancl01@gmail.com>
2018-05-18 06:26:25 +0900

    機能追加(sayCount)

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+require('date-utils');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,12 +17,40 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
+        
+        // ============================================
+        // 日付を出力 
+        // ============================================
+        // var str = 'aaaHello Worldaaa!';
+        var dt = new Date();
+        var y = dt.getFullYear();
+        var m = ("00" + (dt.getMonth()+1)).slice(-2);
+        var d = ("00" + dt.getDate()).slice(-2);
+        var result = y + "/" + m + "/" + d;
+        // Display a message box to the user
+        vscode.window.showInformationMessage(result);
+    });
+
+    let disposable2 = vscode.commands.registerCommand('extension.sayCount', () => {
+        // The code you place here will be executed every time your command is executed
+        
+        // ============================================
+        // 選択した文字数をボップアップ
+        // ============================================
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Selected characters: ' + text.length);
     });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
